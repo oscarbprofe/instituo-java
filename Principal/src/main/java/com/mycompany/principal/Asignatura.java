2c3d0994ed55c7f4a456eef0e6fb61504556b093
Plaza Vespucio <pvespucio@PVAWSB03LC1000.duoc.acad>
2024-09-05 20:20:45 -0400

    abstraccion de clases

---
+++ Principal/src/main/java/com/mycompany/principal/Asignatura.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principal;
+
+/**
+ *
+ * @author pvespucio
+ */
+public class Asignatura {
+    private Estudiante estudiantes[];
+
+    public Asignatura(Estudiante[] estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public Estudiante[] getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(Estudiante[] estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+    
+    
+}
---
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principal;
+
+/**
+ *
+ * @author pvespucio
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
